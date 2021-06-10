eb367d94e634232f49870b34abb29d453a468b99
Noel Gonzalez <noel.argonzalez@gmail.com>
2021-06-09 21:45:26 -0300

    First stable version(v1.0)

--- hangman-game.py
+++ hangman-game.py
@@ -1,6 +1,11 @@
 import random
 import os
+import unicodedata
 
+SELECTED_WORD = ""
+COMPLETE_WORD = []
+
+# Get a random word from the .txt file
 def random_word():
   with open("./data.txt", "r", encoding="utf-8") as f:
     allText = f.read()
@@ -8,31 +13,50 @@ def random_word():
 
   return selectedWord 
 
-def match_letter(letter_entered, word):
-  idx_from_word = []
-  for idx, letter in enumerate(word):
-    if letter == letter_entered:
-      idx_from_word.append(idx)
+def match_letter(letter_entered):
+  global COMPLETE_WORD
 
-  return idx_from_word
+  for idx, letter in enumerate(SELECTED_WORD):
+    if unicodedata.normalize('NFKD', letter).encode('ASCII', 'ignore') == unicodedata.normalize('NFKD', letter_entered).encode('ASCII', 'ignore'):
+      COMPLETE_WORD[idx]=letter
 
+  return COMPLETE_WORD
+
+def draw_new_screen():
+  global COMPLETE_WORD
+  clear_screen()
+  print("¡ Adivina la palabra !")
+
+  for letter in COMPLETE_WORD:
+    print (letter, end = ' ')
+
+# Clear the terminal screen 
 def clear_screen():
   if os.name == "nt":
     os.system("cls")
   else:
     os.system("clear")
 
+# The main function
 def run():
   clear_screen()
   print("¡ Adivina la palabra !")
+  global SELECTED_WORD
+  global COMPLETE_WORD
+  SELECTED_WORD = random_word()
 
-  selectedWord = random_word()
-  underscore = ""
-  for i in range(1, len(selectedWord) + 1):
-      underscore = underscore + "_ "
+  for i in range(1, len(SELECTED_WORD) + 1):
+      COMPLETE_WORD.append("_")
+      print (end = '_ ')
   
-  print(underscore)
-  letter = input("\nIngrese una letra: ")
+  while "_" in COMPLETE_WORD:
+    letter = input("\n\nIngrese una letra: ")
+
+    wordMatch = match_letter(letter)
+    draw_new_screen()
+  
+  clear_screen()
+  print("\n\n¡ GANASTE !")
 
 
 if __name__ == "__main__":
